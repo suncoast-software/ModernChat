807676704980f7e88de6f7b3c004c56d23031d5f
Suncoast Software <53012974+suncoast-software@users.noreply.github.com>
2021-09-09 03:22:55 -0400

    Add project files.

---
+++ ModernChat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,88 @@
+﻿using ModernChat.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModernChat.MVVM.ViewModel
+{
+   public class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+        public ICommand SendCommand { get; set; }
+
+        private ContactModel _selectedContact;
+        public ContactModel SelectedContact
+        {
+            get => _selectedContact;
+            set => OnPropertyChanged(ref _selectedContact, value);
+        }
+
+        private string message;
+        public string Message
+        {
+            get => message;
+            set => OnPropertyChanged(ref message, value);
+        }
+
+        public MainViewModel()
+        {
+            SendCommand = new RelayCommand(Send);
+
+            Messages = new();
+            Contacts = new();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Allison {i}",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Messages = Messages
+                });
+            }
+            LoadMessages();
+        }
+
+        private void Send()
+        {
+            Messages.Add(new MessageModel
+            {
+                Message = Message,
+                FirstMessage = false,
+                
+            });
+            Message = "";
+        }
+
+        private void LoadMessages()
+        {
+            Messages.Add(new MessageModel
+            {
+                Username = "Allison",
+                UsernameColor = "#000",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = $"hello",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+   
+            Messages.Add(new MessageModel
+            {
+                Username = "Timothy",
+                UsernameColor = "#409aff",
+                ImageSource = "https://imgur.com/gallery/wd37E",
+                Message = "hello there",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+        }
+    }
+}
---
+++ ModernChat/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModernChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = Application.Current.MainWindow.WindowState != WindowState.Maximized ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
